8fb4dfe279dd4053fdf57ad19c75e38c06d73229
Maxi Rosson <maxirosson@gmail.com>
2018-03-21 13:20:52 -0300

    Added more Mime Types

--- jdroid-java-http-core/src/main/java/com/jdroid/java/http/MimeType.java
+++ jdroid-java-http-core/src/main/java/com/jdroid/java/http/MimeType.java
@@ -4,7 +4,12 @@ public class MimeType {
 	
 	public static final String TEXT = "text/plain";
 	public static final String HTML = "text/html";
+	public static final String WEBP = "image/webp";
 	public static final String PNG = "image/png";
+	public static final String JPEG = "image/jpeg";
+	public static final String BMP = "image/bmp";
+	public static final String GIF = "image/gif";
+	public static final String IMAGE = "image/*";
 	public static final String JSON = "application/json";
 	public static final String XML = "application/xml";
 	public static final String JSON_UTF8 = "application/json;charset=UTF-8";
