be0ca83dc0414c09ce174f2a670b1f2c7d0758c4
Maxi Rosson <maxirosson@gmail.com>
2019-08-23 21:59:40 -0300

    Migrated to buildSrc

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,11 @@
+object Libs {
+    // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
+    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0-SNAPSHOT"
+
+    // https://github.com/square/okhttp/blob/master/CHANGELOG.md
+    const val okhttp = "com.squareup.okhttp3:okhttp:4.0.0"
+
+    // Testing
+    const val testng = "org.testng:testng:6.8.21"
+    const val mockito = "org.mockito:mockito-all:1.10.19"
+}
