98526a4a38eed8af6cb85ca5d8528e717d5f9410
jdroid-ci <jdroidtools@gmail.com>
2019-09-08 10:57:31 +0000

    Upgraded dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -3,9 +3,9 @@ object Libs {
     const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0"
 
     // https://github.com/square/okhttp/blob/master/CHANGELOG.md
-    const val okhttp = "com.squareup.okhttp3:okhttp:4.0.0"
+    const val okhttp = "com.squareup.okhttp3:okhttp:4.1.1"
 
     // Testing
-    const val testng = "org.testng:testng:6.8.21"
+    const val testng = "org.testng:testng:7.0.0"
     const val mockito = "org.mockito:mockito-all:1.10.19"
 }
