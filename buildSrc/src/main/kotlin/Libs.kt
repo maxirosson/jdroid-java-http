5fb3460477dbd8b6897dc8fcf180dd8090b0b061
Maxi Rosson <maxirosson@gmail.com>
2019-10-01 19:24:05 -0300

    Fixed dependencies format

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,11 +1,11 @@
 object Libs {
     // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
-    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.1.0-SNAPSHOT"
+    const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.1.0-SNAPSHOT"
 
     // https://github.com/square/okhttp/blob/master/CHANGELOG.md
-    const val okhttp = "com.squareup.okhttp3:okhttp:4.2.0"
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:4.2.0"
 
     // Testing
-    const val junit = "junit:junit:4.12"
-    const val mockito = "org.mockito:mockito-all:1.10.19"
+    const val JUNIT = "junit:junit:4.12"
+    const val MOCKITO = "org.mockito:mockito-all:1.10.19"
 }
